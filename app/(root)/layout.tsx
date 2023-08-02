0605ebd1511bc7d532a1b1f9dc4263432cb5158e
jinghao <jinghao@itask.com.sg>
2023-08-02 19:03:07 +0800

    macbook push

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,24 @@
+import { Navbar } from '@/components/navbar'
+import { Sidebar } from '@/components/sidebar'
 import React from 'react'
 
 const RootLayout = ({
-    children
-}:{
-    children: React.ReactNode
+  children
+}: {
+  children: React.ReactNode
 }) => {
   return (
     <div className='h-full'>
-        <main className='md:pl-20 pt-16 h-full'>
-            {children}
-        </main>
+      <Navbar />
+
+      <div className='hidden md:flex mt-16 w-20 flex-col fixed inset-y-0'>
+        <Sidebar />
+      </div>
+
+      <main className='md:pl-20 pt-16 h-full'>
+        {children}
+      </main>
+
     </div>
   )
 }
