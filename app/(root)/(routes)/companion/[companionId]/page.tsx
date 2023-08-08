97ecad719eed1b2e4baa7fc5e9a7ffce57e6b1e7
jinghao <jinghao@itask.com.sg>
2023-08-08 19:34:04 +0800

    -

---
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,13 @@
+interface CompanionIdPageProps {
+    params: {
+        companionId: string;
+    };
+};
+
+const CompanionIdPage = async () => {
+  return (
+    <div>Hello companion ID !</div>
+  )
+}
+
+export default CompanionIdPage
